28cf785289f774a646d2558210c0596be370c054
richardbadman <richardg@brandwatch.com>
2020-09-26 18:56:28 +0100

    Simple usage

--- py-cene/src/main.py
+++ py-cene/src/main.py
@@ -1,8 +1,20 @@
 #!/usr/bin/env python
 
+from index.segment import Segment
+
 
 def main():
-    exit(0)
+    segment = Segment("seg")
+    documents = {
+        1: "Winter is coming",
+        2: "I hate winter coats!",
+        3: "Christmas is, early this year"
+    }
+    
+    for document_id, document in documents.items():
+        segment.write(document, document_id)
+
+    print(segment.get())
 
 
 if __name__ == "__main__":
