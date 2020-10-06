e5833e685a38ac03c72315dc4a2ef0339f8abe23
richardbadman <richardg@brandwatch.com>
2020-10-06 17:55:50 +0100

    Added basic impl. for cache directory

--- py_cene/directory/cache_directory.py
+++ py_cene/directory/cache_directory.py
@@ -1,3 +1,4 @@
+from py_cene.analyser import format_text
 from py_cene.directory import Directory
 from py_cene.index import Index
 
@@ -7,8 +8,36 @@ class CacheDirectory(Directory):
         super().__init__()
         self.indexes = set()
     
-    def get_indexes(self):
-        return self.indexes
+    def get_index_names(self):
+        return {
+            index.get_name()
+            for index in self.indexes
+        }
 
     def create_new_index(self, name):
         self.indexes.add(Index(name))
+
+    def write_to_index(self, index_name, documents):
+        index = self._get_index(index_name)
+        # TODO - implement a writer
+        with index as open_index:
+            for document_id, document in documents:
+                text = format_text(document)
+                open_index.write(document_id, text)
+        
+    def commit_index(self, index_name):
+        index = self._get_index(index_name)
+        index.commit()
+
+    def search(self, term):
+        results = []
+        for index in self.indexes:
+            with index as open_index:
+                results.append(open_index.search(term))
+        return results
+    
+    def _get_index(self, index_name):
+        for index in self.indexes:
+            if index.get_name() == index_name:
+                return index
+        raise ValueError(f"Unable to find index with name: {index_name}")
