7aac781fb95fa63373d60d5a38742ac4760ce115
richardbadman <richardg@brandwatch.com>
2020-10-09 15:26:51 +0100

    Updated main to reflect new changes

--- py_cene/main.py
+++ py_cene/main.py
@@ -2,24 +2,31 @@
 
 from py_cene.analyser import format_text
 from py_cene.directory import DirectoryFactory
+from py_cene.index import (
+    IndexWriter,
+    IndexSearcher
+)
 
 
 def main():
-    documents = {
-        1: "Winter is coming",
-        2: "I hate winter coats!",
-        3: "Christmas is, early this year"
-    }
-
+    documents = [
+        "I've just hung out my washing, it should dry by tomorrow",
+        "I'm really enjoying doing this project and learning more about lucene",
+        "Lucene has been around for a long long time"
+    ]
+    
     directory_factory = DirectoryFactory()
     cache_directory = directory_factory.create("CACHE")
-    cache_directory.create_new_index("test")
+    index_writer = IndexWriter(cache_directory)
+    index_searcher = IndexSearcher(cache_directory)
     
-    cache_directory.write_to_index("test", documents)
-    cache_directory.commit_index("test")
+    for document in documents:
+        index_writer.add_document(document)
+    index_writer.commit()
+
+    documents = index_searcher.search("lucene")
+    print(documents)
     
-    results = cache_directory.search("winter")
-    print(results)
 
 if __name__ == "__main__":
     main()
