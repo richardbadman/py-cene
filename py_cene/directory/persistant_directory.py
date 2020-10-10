b4041050d372a778ce8bd3494a5053b1e1a8707e
richardbadman <richardg@brandwatch.com>
2020-10-10 13:14:14 +0100

    Added writing logic for PersistantDirectory
    This saves raw documents to a file where its a new line containing
    {doc_id: doc}
    in a compressed byte format
    When index is comitted, writes index object to file in byte format also
    
    Tests not yet implemented
    Search still needs to be implemented

--- py_cene/directory/persistant_directory.py
+++ py_cene/directory/persistant_directory.py
@@ -1,9 +1,97 @@
-from py_cene.directory import Directory
+import io
+import pickle
+import uuid
+import zlib
 
+from os import listdir, linesep, path
+
+from py_cene.directory.directory import Directory
+from py_cene.index import Index
+
+
+OPEN_DOCUMENT_EXTENSION = ".fdt"
+CLOSED_DOCUMENT_EXTENSION = ".fdx"
+OPEN_INDEX_EXTENSION = ".tim"
+CLOSED_INDEX_EXTENSION = ".tip"
 
 class PersistantDirectory(Directory):
     def __init__(self):
-        pass
+        # TODO - Implement size capping
+        super().__init__()
+        self.directory_path = None
+        self.open_index = None
+
+        self.open_document_file = None
+        self.open_index_file = None
+
+        self.closed_document_files = []
+        self.closed_index_files = []
     
-    def create(self):
-        pass
+    def get_directory(self, directory_path):
+        if path.exists(directory_path) and path.isdir(directory_path):
+            all_files = _get_files_in_path(directory_path)
+            self.open_document_file = _get_open_file(_get_by_extension(all_files, OPEN_DOCUMENT_EXTENSION))
+            self.open_index_file = _get_open_file(_get_by_extension(all_files, OPEN_INDEX_EXTENSION))
+            
+            self.closed_document_files = _get_by_extension(all_files, CLOSED_DOCUMENT_EXTENSION)
+            self.closed_index_files = _get_by_extension(all_files, CLOSED_INDEX_EXTENSION)
+        else:
+            raise ValueError(f"Path doesn't appear to be a directory: {directory_path}")
+
+    def write_to_index(self, document_id, text):
+        if not self.open_index_file:
+            self._get_new_file(OPEN_INDEX_EXTENSION, self.open_index_file)
+            self.open_index = Index()
+        with self.open_index as open_index:
+            open_index.write(document_id, text)
+
+    def commit(self):
+        self.open_index.commit()
+        _write_data_to_file(self.open_index_file, self.open_index)
+
+    def append_document(self, document_id, document):
+        if not self.open_document_file:
+            self._get_new_file(OPEN_DOCUMENT_EXTENSION, self.open_document_file)
+        _write_data_to_file(self.open_document_file, {document_id: document})
+    
+    def _get_new_file(self, extension, current_file):
+        # TODO - Check if it already exists
+        file_name = uuid.uuid4().hex[:6]
+        complete_path = f"{self.directory_path}/{file_name}{extension}"
+        with open(complete_path, "wb+"): current_file = complete_path
+        
+    
+
+def _get_files_in_path(directory_path):
+    return [
+        file for file in listdir(directory_path)
+        if path.isfile(
+            path.join(directory_path, file)
+        )
+    ]
+
+
+def _get_by_extension(files, extension):
+    return [
+        file 
+        for file in files
+        if file.endswith(extension)
+    ]
+
+def _get_open_file(open_files):
+    if len(open_files) > 1:
+        raise ValueError("Corrupt Index! There appears to be more than 1 open file of the same type.")
+    if len(open_files) == 1:
+        return open_files[0]
+    return None
+
+    
+def _write_data_to_file(data, filename):
+    # TODO - Should go in the IndexWriter?
+    data_as_bytes = io.BytesIO()
+    pickle.dump(data, data_as_bytes)
+    
+    compressed_bytes = zlib.compress(data_as_bytes.getbuffer())
+    with open(filename, "ab") as open_document_file:
+        open_document_file.write(compressed_bytes)
+        open_document_file.write(linesep.encode())
