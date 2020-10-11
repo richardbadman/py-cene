aa6af72454a57ae1a08d0ee0db24dee72072ce03
richardbadman <richardg@brandwatch.com>
2020-10-11 20:01:03 +0100

    Implemented persistent saving and loading
    Massive work here, I've got an ugly (atm) impl. of making it save the
    index object(s) and documents to separate files on disk.
    
    If you then create a new PersistantDirectory type from the
    DirectoryFactory (as done in the tests) it successfully loads them, and
    it's possible to then search the persisting data
    
    It's quite hacky at the moment though, lots of room for improvements.
    But happy it works

--- py_cene/directory/persistant_directory.py
+++ py_cene/directory/persistant_directory.py
@@ -29,38 +29,61 @@ class PersistantDirectory(Directory):
     
     def get_directory(self, directory_path):
         # TODO - Need to figure out how to call this better on object creation
+        # TODO - load Index if already exists
         if path.exists(directory_path) and path.isdir(directory_path):
             self.directory_path = directory_path
 
             all_files = _get_files_in_path(directory_path)
-            self.open_document_file = _get_open_file(
+            open_document_file = _get_open_file(
                 _get_by_extension(all_files, OPEN_DOCUMENT_EXTENSION)
             )
-            self.open_index_file = _get_open_file(
+            if open_document_file:
+                self.open_document_file = open_document_file
+            else:
+                self.open_document_file = self._get_new_file(OPEN_DOCUMENT_EXTENSION)
+
+            open_index_file = _get_open_file(
                 _get_by_extension(all_files, OPEN_INDEX_EXTENSION)
             )
+            if open_index_file:
+                self.open_index = _read_data_from_file(open_index_file, index_file=True)
+                self.open_index_file = open_index_file
+            else:
+                self.open_index = Index()
+                self.open_index_file = self._get_new_file(OPEN_INDEX_EXTENSION)
 
             self.closed_document_files = _get_by_extension(all_files, CLOSED_DOCUMENT_EXTENSION)
             self.closed_index_files = _get_by_extension(all_files, CLOSED_INDEX_EXTENSION)
         else:
-            raise ValueError(f"Path doesn't appear to be a directory: {directory_path}")
+            # TODO - Change exception
+            raise ValueError(f"Path doesn't exist, or doesn't appear to be a directory: {directory_path}")
 
     def write_to_index(self, document_id, text):
-        if not self.open_index_file:
-            self._get_new_file(OPEN_INDEX_EXTENSION)
-            self.open_index = Index()
         with self.open_index as open_index:
             open_index.write(document_id, text)
 
     def commit(self):
         self.open_index.commit()
-        _write_data_to_file(self.open_index_file, self.open_index)
+        _write_data_to_file(self.open_index_file, self.open_index, index_file=True)
 
     def append_document(self, document_id, document):
         # TODO - call this when committing, and flush to rest of documents
-        if not self.open_document_file:
-            self.open_document_file = self._get_new_file(OPEN_DOCUMENT_EXTENSION)
         _write_data_to_file(self.open_document_file, {document_id: document})
+
+    def search_index(self, term):
+        # TODO - search closed indexes
+        with self.open_index as open_index:
+            return open_index.search(term)
+        
+    def get_documents(self, document_ids):
+        # TODO - search closed documents
+        return [
+            document
+            for document_id, document in _format_document_data(
+                _read_data_from_file(self.open_document_file)
+            ).items()
+            if document_id in document_ids
+        ]
     
     def _get_new_file(self, extension):
         # TODO - Check if it already exists
@@ -73,7 +96,7 @@ class PersistantDirectory(Directory):
 
 def _get_files_in_path(directory_path):
     return [
-        file for file in listdir(directory_path)
+        f"{directory_path}/{file}" for file in listdir(directory_path)
         if path.isfile(
             path.join(directory_path, file)
         )
@@ -95,7 +118,7 @@ def _get_open_file(open_files):
     return None
 
     
-def _write_data_to_file(filename, data):
+def _write_data_to_file(filename, data, index_file=False):
     # TODO - Should go in the IndexWriter?
     data_as_bytes = io.BytesIO()
     pickle.dump(data, data_as_bytes)
@@ -103,8 +126,36 @@ def _write_data_to_file(filename, data):
     compressed_bytes = zlib.compress(data_as_bytes.getbuffer())
     with open(filename, "ab") as open_document_file:
         open_document_file.write(compressed_bytes)
-        open_document_file.write(linesep.encode())
+        if not index_file:
+            open_document_file.write(linesep.encode())
 
+
+def _read_data_from_file(filename, index_file=False):
+    # TODO - Should go in the IndexSearcher?
+    # TODO - yield?
+    # TODO - FIX THIS MASSIVELY
+    document_data = []
+    with open(filename, "rb") as open_file:
+        if not index_file:
+            for compressed_bytes in open_file:
+                data_as_bytes = zlib.decompress(compressed_bytes)
+                data = pickle.loads(data_as_bytes)
+                document_data.append(data)
+        else:
+            compressed_bytes = open_file.read()
+            data_as_bytes = zlib.decompress(compressed_bytes)
+            data = pickle.loads(data_as_bytes)
+            return data
+            
+    return document_data
+
+
+def _format_document_data(document_data):
+    return {
+        document_id: document
+        for data in document_data
+        for document_id, document in data.items()
+    }
 """
 In [64]: def file2dict(filename):
     ...:     with open(filename, 'rb') as fd:
--- tests/persistant_directory_test.py
+++ tests/persistant_directory_test.py
@@ -21,39 +21,73 @@ class PersistantDirectoryTest(unittest.TestCase):
 
     def setUp(self):
         os.mkdir(TEMP_DIRECTORY)
+        self.persistant_directory = DIRECTORY_FACTORY.create("PERSISTANT")
+        self.persistant_directory.get_directory(TEMP_DIRECTORY)
         
     def tearDown(self):
         rmtree(TEMP_DIRECTORY)
 
     def test_writing_to_index(self):
-        persistant_directory = DIRECTORY_FACTORY.create("PERSISTANT")
-        persistant_directory.get_directory(TEMP_DIRECTORY)
-        
-        persistant_directory.write_to_index(1, TEXT) 
+        self.persistant_directory.write_to_index(1, TEXT) 
 
     def test_appending_documents(self):
-        persistant_directory = DIRECTORY_FACTORY.create("PERSISTANT")
-        persistant_directory.get_directory(TEMP_DIRECTORY)
-        
-        persistant_directory.append_document(1, DOCUMENT)
+        self.persistant_directory.append_document(1, DOCUMENT)
         
     def test_when_commiting_index_with_no_data_then_exception_is_met(self):
-        # TODO
-        pass
-        # persistant_directory = DIRECTORY_FACTORY.create("PERSISTANT")
-        # persistant_directory.get_directory(TEMP_DIRECTORY)
-
-        # with raises(ValueError):
-        #     persistant_directory.commit()
+        with raises(ValueError):
+            self.persistant_directory.commit()
 
     def test_searching_index(self):
-        pass
+        self.persistant_directory.write_to_index(1, TEXT)
+        self.persistant_directory.commit()
+
+        expected_results = {1: 1}
+        actual_results = self.persistant_directory.search_index("text")
+        assert expected_results == actual_results
 
     def test_getting_documents(self):
-        pass
+        self.persistant_directory.write_to_index(1, TEXT)
+        self.persistant_directory.append_document(1, DOCUMENT)
+        self.persistant_directory.commit()
+
+        expected_results = ["A sample text."]
+        actual_results = self.persistant_directory.get_documents([1])
+        assert expected_results == actual_results
 
     def test_when_creating_entirely_new_persistant_directory_no_data_is_there(self):
-        pass
+        expected_results = dict()
+        actual_results = self.persistant_directory.search_index("term")
+        assert expected_results == actual_results
 
-    def test_when_opening_persistant_directory_that_already_exists_on_disk_data_is_there(self):
-        pass
+    def test_when_opening_persistant_directory_that_already_exists_on_disk_then_data_is_there(self):
+        self.persistant_directory.write_to_index(1, TEXT)
+        self.persistant_directory.append_document(1, DOCUMENT)
+        self.persistant_directory.commit()
+
+        self.persistant_directory = None
+        self.persistant_directory = DIRECTORY_FACTORY.create("PERSISTANT")
+        self.persistant_directory.get_directory(TEMP_DIRECTORY)
+        
+        expected_results = {1: 1}
+        actual_results = self.persistant_directory.search_index("text")
+        assert expected_results == actual_results
+
+    def test_writing_multiple_documents_and_then_checking_they_persist(self):
+        documents = {
+            1: "A sample text.",
+            2: "Another text line"
+        }
+        
+        for document_id, document in documents.items():
+            text = format_text(document)
+            self.persistant_directory.write_to_index(document_id, text)
+            self.persistant_directory.append_document(1, document)
+        self.persistant_directory.commit()
+
+        self.persistant_directory = None
+        self.persistant_directory = DIRECTORY_FACTORY.create("PERSISTANT")
+        self.persistant_directory.get_directory(TEMP_DIRECTORY)
+        
+        expected_results = {1: 1, 2: 1}
+        actual_results = self.persistant_directory.search_index("text")
+        assert expected_results == actual_results
