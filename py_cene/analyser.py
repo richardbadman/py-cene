38f8b77092792084f47cc4170ae3cec12b749f31
richardbadman <richardg@brandwatch.com>
2020-11-08 13:54:40 +0000

    Made analyser a class instead
    This will then be moved into an analyser sub module in future, but for
    now its okay being where it is

--- py_cene/analyser.py
+++ py_cene/analyser.py
@@ -5,11 +5,15 @@ from py_cene.stopwords import STOPWORDS
 
 PUNCTUATION_REGEX = r"[^\w\s]"
 
-def format_text(text):
-    text_to_lower = text.lower().rstrip()
-    text_with_no_punctuation = re.sub(PUNCTUATION_REGEX, "", text_to_lower)
-    words = text_with_no_punctuation.split()
-    return [
-        word for word in words if words
-        if word not in STOPWORDS
-    ]
+class StandardAnalyser:
+    def __init__(self):
+        pass
+
+    def format_text(self, text):
+        text_to_lower = text.lower().rstrip()
+        text_with_no_punctuation = re.sub(PUNCTUATION_REGEX, "", text_to_lower)
+        words = text_with_no_punctuation.split()
+        return [
+            word for word in words if words
+            if word not in STOPWORDS
+        ]
